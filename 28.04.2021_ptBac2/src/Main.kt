2f06d8d97dc55e9dcf66e901173e86a36b02f9de
SNKz0610 <blackaxe2202@gmail.com>
2021-04-28 13:40:52 +0700

    chore():task#2804 Adding Android Homework 28/04/2021 about solving quadratic. Closes task#2804

---
+++ 28.04.2021_ptBac2/src/Main.kt
@@ -0,0 +1,48 @@
+package ptbac2
+
+
+fun main() {
+    while (true) {
+        println("Giai phuong trinh bac 2: a.x^2 + b.x + c = 0")
+        println("Nhap a:")
+        val a: Float? = readLine()?.toFloat()
+        println("Nhap b:")
+        val b: Float? = readLine()?.toFloat()
+        println("Nhap c:")
+        val c: Float? = readLine()?.toFloat()
+
+        // Xu ly a = 0
+        if (a == 0.toFloat()) {
+            val result: Float = -b?.let { c?.div(it) }!!
+            println("> Phuong trinh co 1 nghiem duy nhat x = $result")
+            println("=================================")
+        }
+        //Xu ly cac truong hop cua Delta
+        else {
+            //Tinh delta
+            val delta1: Float = (b!! * b!!) - (4 * a!! * c!!)
+            
+            //Delta = 0
+            if (delta1 == 0.toFloat()) {
+                val result1: Float = -b!! / (2 * a!!)
+                println("Phuong trinh co nghiem kep la x1 = x2 = $result1")
+                println("=================================")
+            }
+            //Delta < 0
+            else if (delta1 < 0.toFloat()) {
+                println("=> Phuong trinh vo nghiem!")
+                println("=================================")
+            }
+            //Delta > 0
+            else if (delta1 > 0.toFloat()) {
+                println("=>Phuong trinh co 2 nghiem phan biet: ")
+                val sqrtDelta: Float = Math.sqrt(delta1.toDouble()).toFloat()
+                val x1: Float = (-b!! - sqrtDelta) / (2 * a!!)
+                val x2: Float = (-b!! + sqrtDelta) / (2 * a!!)
+                println("+x1 = $x1")
+                println("+x1 = $x2")
+                println("=================================")
+            }
+        }
+    }
+}
